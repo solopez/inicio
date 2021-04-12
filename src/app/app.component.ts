50812e92611942eecd8066bba67b66b43d12ac14
LOPEZ Sol <LopezS@sancristobal.com.ar>
2021-04-12 16:22:55 -0300

    pushing todos base structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  list = [];
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      item: new FormControl()
+    });
+  }
+
+  addItem() {
+    const item = {
+      value: this.form.get('item').value,
+      completed: false
+    };
+    this.list.push(item);
+  }
+
+  deleteItem(position) {
+    this.list.splice(position, 1);
+  }
+
+  completeTask(position) {
+    this.list[position].completed = true;
+  }
 }
